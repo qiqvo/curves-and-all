3a2e3f09ac9afe9b14d1fa0d4d5208132f2ede1d
Ben Povar <ben.povar@gmail.com>
2020-12-02 21:08:36 +0200

    simplifications

--- curve.py
+++ curve.py
@@ -1,5 +1,5 @@
 import attr
-import typing 
+from typing import * 
 import pandas as pd
 import numpy as np
 
@@ -9,37 +9,21 @@ import matplotlib.pyplot as plt
 
 @attr.s(slots=True, auto_attribs=True)
 class Curve():
-	"""
-	Curve has elements:
-		basis
-		interpolation
-		data
-	"""
-	data : pd.DataFrame = attr.ib(default=None)
-	basis : Basis = attr.ib(default=None)
+	times : List[float] = attr.ib(default=None)
+	data : List[float] = attr.ib(default=None)
 	interpolation : Interpolation = attr.ib(default=None)
-	name : str = attr.ib(default=None)
 
-	def __attrs_post_init__(self):
-		self.data.set_index("Date")
-		dates = self.data.index
-		times = [self.basis.get_time(date) for date in dates]
-		self.data.insert(0, "Time", times, True)
-		self.data.rename(columns={self.name : 'Value'}, inplace=True)
-		# self.interpolation = LinearInterpolation(self.data)
-
-	def value(self, date=None, time=None):
-		if date is not None:
-			time = self.basis.get_time(date)
+	def value(self, time : float):
 		return self.interpolation.value(time)
 
-	def plot(self, end_date):
-		time0 = self.basis.today
-		time1 = self.basis.get_time(end_date)
+	def values(self, times : List[float]):
+		return np.array([self.value(time) for time in times])
 
-		times = np.linspace(time0, time1)
-		plt.plot(times, self.value(time=times))
-		plt.scatter(self.data["Time"], self.data['Value'], c='r')
+	def plot(self):
+		times = np.linspace(self.times[0], self.times[-1], 100)
+		
+		plt.plot(times, self.values(times))
+		plt.scatter(self.times, self.data, c='r')
 		# plt.show()
 
 @attr.s(slots=True, auto_attribs=True)
@@ -48,7 +32,7 @@ class LinearCurve(Curve):
 	Curve with Log linear interpolation.
 	"""
 	def __attrs_post_init__(self):
-		self.interpolation = LinearInterpolation(self.data)
+		self.interpolation = LinearInterpolation(data=self.data, times=self.times)
 
 @attr.s(slots=True, auto_attribs=True)
 class LogLinearCurve(Curve):
@@ -56,31 +40,4 @@ class LogLinearCurve(Curve):
 	Curve with Log linear interpolation.
 	"""
 	def __attrs_post_init__(self):
-		self.interpolation = LogLinearInterpolation(self.data)
-
-
-def create_basis_curve():
-	data = np.array([["2020-09-18", 1.0], 
-						["2020-12-20", 0.998982370689909],
-						["2021-06-20", 0.997500050423405],
-						["2022-06-20", 0.989998832273272]])
-	data = pd.DataFrame(data, columns=['Date', 'Sp'])
-	dates = pd.to_datetime(data["Date"].values)
-
-	data.drop(columns=["Date"], inplace=True)
-	data.insert(0, "Date", dates, True)
-	data['Sp'] = data['Sp'].astype(float)
-
-	basis = BasisAct360(pd.to_datetime("2020-09-18"))
-	sp_curve = LogLinearCurve(data, basis, 'Sp')
-	
-	return basis, sp_curve
-
-def test():
-	"""Test."""
-	basis, sp_curve = create_basis_curve()
-	
-	print(pd.to_datetime("2020-10-15"), basis.get_time(pd.to_datetime("2020-10-15")), sp_curve.value(pd.to_datetime("2020-10-15")))
-	print(pd.to_datetime("2021-01-15"), basis.get_time(pd.to_datetime("2021-01-15")), sp_curve.value(pd.to_datetime("2021-01-15")))
-	print(pd.to_datetime("2021-04-15"), basis.get_time(pd.to_datetime("2021-04-15")), sp_curve.value(pd.to_datetime("2021-04-15")))
-	print(pd.to_datetime("2022-04-15"), basis.get_time(pd.to_datetime("2022-04-15")), sp_curve.value(pd.to_datetime("2022-04-15")))
+		self.interpolation = LogLinearInterpolation(data=self.data, times=self.times)
--- main.py
+++ main.py
@@ -1,29 +1,71 @@
 import attr
-import typing 
 import pandas as pd
 import numpy as np
+from typing import * 
+
+from scipy.optimize import root
 
 from curve import *
-from trade import *
-from iterative_bootstrap import *
 
-timetable = np.linspace(0.1, 20, 30)
+timetable = np.linspace(0.1, 8, 20)
 
 def trade1(curves):
+    v = -0.2931530452338909
+    times = timetable[:10:2]
+    return np.sum(2 * curves[0].values(times)**2 -  curves[1].values(times)**3) - v
+
+def trade2(curves):
+    v = -359.1314035087719
     times = timetable[::2]
-    return [curves[0].value(time) for time in times]
+    return np.sum(100*curves[0].values(times) - 101 * curves[1].values(times)) - v
+
+def trade3(curves):
+    v = 
+    times = timetable[5::2]
+    return np.sum(100*curves[0].values(times) - 101 * curves[1].values(times)) - v
+
+def main3():
+    values_curve1 = [1, 0.4, 0.39, 0.29]
+    times_curve1 =  [0, 1, 2, 8]
+    curve1 = LinearCurve(times=times_curve1, data=values_curve1)
+    
+    values_curve2 = [1, 0.9, 0.8, 0.7, 0.6]
+    times_curve2 =  [0, 1.5, 2, 5, 8]
+    curve2 = LinearCurve(times=times_curve2, data=values_curve2)
+
+    print("trade1", trade1([curve1, curve2]))
+    print("trade2", trade2([curve1, curve2]))
+
+    curve1.plot()
+    curve2.plot()
+    plt.show()
 
 def main():
-    basis, curve = create_basis_curve()
-    today = basis.today
+    times = [0, timetable[9], timetable[-1]]
+    values = [0.9**i for i in range(len(times))]
 
-    trade1 = LiborTrade(basis=basis, quote=0.035, \
-                quantity=1, cashflow_dates=[today + pd.Timedelta(days=100)])
-    trade2 = FuturesTrade(basis=basis, quote=0.03, \
-                quantity=1, \
-                cashflow_dates=[today + pd.Timedelta(days=100), today + pd.Timedelta(days=200)])
-    
-    
+    curves = [LinearCurve(times=times[:], data=values[:]),
+                LinearCurve(times=times[:], data=values[:])]
+
+    trades = [trade1, trade2]
+
+    time_places = [1, 2]
+
+    N = 100
+    i = 0
+    while i < N: 
+        i += 1
+        for curve in curves:
+            for j in range(len(trades)):
+                def func(x):
+                    curve.data[time_places[j]] = x[0]
+                    return trades[j](curves)
+                z = root(func, 0.5).x
+                print(curve.data)
+        curves[0].plot()
+        curves[1].plot()
+        plt.show()
+
 
 if __name__ == "__main__":
     main()
