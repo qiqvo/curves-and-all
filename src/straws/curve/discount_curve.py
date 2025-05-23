4ea931abd473cfca758b9dadb2a055734fd2c8e0
Ben Povar <ben.povar@gmail.com>
2025-05-23 22:09:07 +0100

    Discount between dates.

--- src/straws/curve/discount_curve.py
+++ src/straws/curve/discount_curve.py
@@ -9,6 +9,12 @@ class DiscountCurve(Curve):
     def discount(self, time: float) -> float:
         return self.evaluate(time)
     
-    def short_rate(self, time: float) -> float:
+    def short_rate(self,  time: float) -> float:
         # Calculate the short rate from the discount factor
         return -np.log(self.discount(time)) / time
+    
+    def discount_on_date(self, on_date, date=None):
+        if date is None:
+            return self.evaluate_on_date(on_date)
+        else:
+            return self.evaluate_on_date(date) / self.evaluate_on_date(on_date)
